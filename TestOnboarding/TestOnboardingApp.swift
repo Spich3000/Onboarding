e5f9ef456edd9cada6197c3aad16f78c1c4306b5
Spich <spichakovda@gmail.com>
2022-06-22 17:57:50 +0300

    Initial Commit

---
+++ TestOnboarding/TestOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOnboardingApp.swift
+//  TestOnboarding
+//
+//  Created by Дмитрий Спичаков on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
